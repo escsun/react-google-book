0a1afd7a372c0e665aea6a848c326d981c15a575
escsun <escsunarcher@gmail.com>
2017-12-22 13:24:59 +0200

    add books constants

---
+++ src/app/core/constants/books.ts
@@ -0,0 +1,8 @@
+enum GoogleBooks {
+  GOOGLE_BOOKS_QUERY = "[Google] Books Query",
+  GOOGLE_BOOKS_QUERY_COMPLETE = "[Google] Books Query Complete",
+  GOOGLE_BOOKS_QUERY_ERROR = "[Google] Books Query Error",
+  GOOGLE_BOOKS_SELECT = "[Google] Books Select"
+}
+
+export default GoogleBooks;
