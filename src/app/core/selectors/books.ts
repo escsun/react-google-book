caea30a581930c173db2be5a81d59e8a2cf388e7
escsun <escsunarcher@gmail.com>
2017-12-22 23:48:39 +0200

    fix getBooks selectors

--- src/app/core/selectors/books.ts
+++ src/app/core/selectors/books.ts
@@ -1,5 +1,5 @@
 import { RootState } from "../reducers";
 
-export const getBooks = (state: RootState) => state.books;
+export const getBooks = (state: RootState) => state.books.books;
 export const getBookById = (state: RootState) => state.books.bookById;
 export const getErrorBookLoading = (state: RootState) => state.books.errorBookLoading;
