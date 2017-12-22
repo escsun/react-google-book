355f47e7b42b644d8fc98961d39c3e65af514d1c
escsun <escsunarcher@gmail.com>
2017-12-22 19:25:22 +0200

    update constants

--- src/app/core/constants/api.ts
+++ src/app/core/constants/api.ts
@@ -1,6 +1,4 @@
-enum ApiEndpointUrl {
+export enum ApiEndpointUrl {
   GOOGLE_BOOKS_QUERY = "https://www.googleapis.com/books/v1/volumes?q=",
-  GOOGLE_BOOKS_SELECT = "https://www.googleapis.com/books/v1/volumes/"
+  GOOGLE_BOOKS_SELECT = "https://www.googleapis.com/books/v1/volumes"
 }
-
-export default ApiEndpointUrl;
--- src/app/core/constants/books.ts
+++ src/app/core/constants/books.ts
@@ -1,8 +1,8 @@
-enum GoogleBooks {
+export enum BooksConstants {
   GOOGLE_BOOKS_QUERY = "[Google] Books Query",
   GOOGLE_BOOKS_QUERY_COMPLETE = "[Google] Books Query Complete",
   GOOGLE_BOOKS_QUERY_ERROR = "[Google] Books Query Error",
-  GOOGLE_BOOKS_SELECT = "[Google] Books Select"
+  GOOGLE_BOOKS_RETRIEVE = "[Google] Books Retrieve",
+  GOOGLE_BOOKS_RETRIEVE_SUCCESS = "[Google] Books Retrieve Success",
+  GOOGLE_BOOKS_RETRIEVE_ERROR = "[Google] Books Retrieve Error"
 }
-
-export default GoogleBooks;
