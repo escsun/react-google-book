187ddba046ee6978d3a4295b50e3f2c5306bbeec
escsun <escsunarcher@gmail.com>
2017-12-22 19:22:04 +0200

    Add Book component

--- src/app/components/Book/index.tsx
+++ src/app/components/Book/index.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 
-const Book = () => {
+import BookModel from "../../core/models/book.model";
+
+interface Props {
+  book: BookModel;
+}
+
+const Book = (props: Props) => {
+  const {book} = props;
   return (
     <div className="_book">
-      this is component for a book
+      <h1>{book.volumeInfo.title}</h1>
+      <p>{book.volumeInfo.description}</p>
     </div>
   );
 };
