6c32d1c3bb2efc9f17ef246e8e5ddc6b1457247b
escsun <escsunarcher@gmail.com>
2017-12-22 13:24:46 +0200

    add api constants

---
+++ src/app/core/constants/api.ts
@@ -0,0 +1,6 @@
+enum ApiEndpointUrl {
+  GOOGLE_BOOKS_QUERY = "https://www.googleapis.com/books/v1/volumes?q=",
+  GOOGLE_BOOKS_SELECT = "https://www.googleapis.com/books/v1/volumes/"
+}
+
+export default ApiEndpointUrl;
