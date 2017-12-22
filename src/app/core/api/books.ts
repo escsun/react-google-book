eeed99c73056279b46d3235442bdc3f5757fd727
escsun <escsunarcher@gmail.com>
2017-12-22 22:22:21 +0200

    fix book api throw error

--- src/app/core/api/books.ts
+++ src/app/core/api/books.ts
@@ -12,5 +12,7 @@ export const fetchGoogleBooksByQuery = () => {
 export const retrieveGoogleBook = (volumeId: string) => {
   return axios.get(`${ApiEndpointUrl.GOOGLE_BOOKS_SELECT}/${volumeId}`)
     .then((response: AxiosResponse) => response.data)
-    .catch((error: AxiosError) => error);
+    .catch((error: AxiosError) => {
+      throw error.response.data.error.message;
+    });
 };
