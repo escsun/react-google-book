2b60fca5f965b56f7b630bbde004dc724c48cc30
escsun <escsunarcher@gmail.com>
2017-12-22 22:00:54 +0200

    add error state for book reducer

--- src/app/core/reducers/books.ts
+++ src/app/core/reducers/books.ts
@@ -6,19 +6,21 @@ import Book from "../models/book.model";
 export interface BooksState {
   books: Book[];
   bookById: Book;
+  errorBookLoading: string;
 }
 
 const initialState: BooksState = {
   books: [],
   bookById: null,
+  errorBookLoading: null
 };
 
 export const booksReducer = (state = initialState, action: BooksAction) => {
   switch (action.type) {
-    // case BooksConstants.GOOGLE_BOOKS_RETRIEVE:
-    //   return state;
     case BooksConstants.GOOGLE_BOOKS_RETRIEVE_SUCCESS:
       return {...state, bookById: action.payload};
+    case BooksConstants.GOOGLE_BOOKS_RETRIEVE_ERROR:
+      return {...state, errorBookLoading: action.payload};
     // case BooksConstants.GOOGLE_BOOKS_QUERY_COMPLETE:
     //   return {...state, books: action.payload };
     default:
