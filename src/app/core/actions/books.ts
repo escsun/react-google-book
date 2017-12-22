4fddcb4bf8621009bd150a1521ec4acb9989a077
escsun <escsunarcher@gmail.com>
2017-12-22 19:26:20 +0200

    add book actions

---
+++ src/app/core/actions/books.ts
@@ -0,0 +1,60 @@
+import { BooksConstants } from "../constants";
+
+import Book from "../models/book.model";
+
+interface GoogleBooksQuery {
+  type: BooksConstants.GOOGLE_BOOKS_QUERY;
+  payload: string;
+}
+
+interface GoogleBooksQueryComplete {
+  type: BooksConstants.GOOGLE_BOOKS_QUERY_COMPLETE;
+  payload: Book[];
+}
+
+interface GoogleBooksQueryError {
+  type: BooksConstants.GOOGLE_BOOKS_QUERY_ERROR;
+  payload: string;
+}
+
+interface GoogleBooksRetrieve {
+  type: BooksConstants.GOOGLE_BOOKS_RETRIEVE;
+  payload: string;
+}
+interface GoogleBooksRetrieveSuccess {
+  type: BooksConstants.GOOGLE_BOOKS_RETRIEVE_SUCCESS;
+  payload: Book;
+}
+
+interface GoogleBooksRetrieveError {
+  type: BooksConstants.GOOGLE_BOOKS_RETRIEVE_ERROR;
+  payload: string;
+}
+
+export const googleBooksRetrieveSuccess = (payload: Book): GoogleBooksRetrieveSuccess => {
+  return {
+    type: BooksConstants.GOOGLE_BOOKS_RETRIEVE_SUCCESS,
+    payload: payload
+  };
+};
+// TODO add payload type error
+export const googleBooksRetrieveError = (payload: any): GoogleBooksRetrieveError => {
+  return {
+    type: BooksConstants.GOOGLE_BOOKS_RETRIEVE_ERROR,
+    payload: payload
+  };
+};
+
+export const googleBooksRetrieve = (payload: string): GoogleBooksRetrieve => {
+  return {
+    type: BooksConstants.GOOGLE_BOOKS_RETRIEVE,
+    payload: payload
+  };
+};
+
+export type BooksAction =
+  | GoogleBooksQuery
+  | GoogleBooksQueryComplete
+  | GoogleBooksQueryError
+  | GoogleBooksRetrieve
+  | GoogleBooksRetrieveSuccess;
