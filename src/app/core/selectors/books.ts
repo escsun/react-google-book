47e16f362fe2d8ad1b9fd3d1acb34230168446bc
escsun <escsunarcher@gmail.com>
2017-12-22 22:01:09 +0200

    add error selectors

--- src/app/core/selectors/books.ts
+++ src/app/core/selectors/books.ts
@@ -2,3 +2,4 @@ import { RootState } from "../reducers";
 
 export const getBooks = (state: RootState) => state.books;
 export const getBookById = (state: RootState) => state.books.bookById;
+export const getErrorBookLoading = (state: RootState) => state.books.errorBookLoading;
