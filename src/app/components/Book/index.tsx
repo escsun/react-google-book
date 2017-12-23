193c4751b739e3878ac5b2dca06c6eb98ee16168
escsun <escsunarcher@gmail.com>
2017-12-23 02:55:21 +0200

    update Book add more information about books

--- src/app/components/Book/index.tsx
+++ src/app/components/Book/index.tsx
@@ -1,17 +1,40 @@
 import * as React from "react";
 
-import BookModel from "../../core/models/book.model";
+import IBook from "../../core/models/book.model";
+
+import "./index.scss";
 
 interface Props {
-  book: BookModel;
+  book: IBook;
+  single?: boolean;
 }
+
+const createMarkup = (html: string) => {
+  return {
+    __html: html
+  };
+};
 
 const Book = (props: Props): JSX.Element => {
   const {book} = props;
+  const {title, subtitle, authors, description} = book.volumeInfo;
+  const {thumbnail, smallThumbnail} = book.volumeInfo.imageLinks;
+  const desc = props.single ? createMarkup(description) : createMarkup(description.substring(0, 150) + "...");
   return (
     <div className="_book">
-      <h1>{book.volumeInfo.title}</h1>
-      <p dangerouslySetInnerHTML={{__html: props.book.volumeInfo.description}}/>
+      <div className="container-book">
+        <h1 className="title">{title}</h1>
+        <h2 className="subtitle">{subtitle}</h2>
+        <div className="container">
+          <img className="image" src={smallThumbnail}/>
+          <div>
+            <ul>
+              {authors.map((author: string, i: number) => <li key={i}>{author}</li>)}
+            </ul>
+          </div>
+        </div>
+        <p className="description" dangerouslySetInnerHTML={desc}/>
+      </div>
     </div>
   );
 };
