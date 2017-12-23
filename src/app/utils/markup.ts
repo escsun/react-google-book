6d7c6f01f2f74fe7061740a008881eeaec641b52
escsun <escsunarcher@gmail.com>
2017-12-23 04:05:57 +0200

    add markup utils

---
+++ src/app/utils/markup.ts
@@ -0,0 +1,11 @@
+export const createMarkup = (html: string) => {
+  return {
+    __html: html
+  };
+};
+
+export const createMarkupShorter = (html: string, truncateTo: number) => {
+  return {
+    __html: html.substring(0, truncateTo)
+  };
+};
