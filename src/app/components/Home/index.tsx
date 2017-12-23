bcedf05ff2482d1711176d9fbcf32d886dc40909
escsun <escsunarcher@gmail.com>
2017-12-23 02:56:53 +0200

    add styles for Home

--- src/app/components/Home/index.tsx
+++ src/app/components/Home/index.tsx
@@ -3,12 +3,16 @@ import * as React from "react";
 import BookSearchPage from "../../containers/BookSearchPage";
 import BooksPage from "../../containers/BooksPage";
 
+import "./index.scss";
+
 const Home = (): JSX.Element => {
   return (
     <div className="_home">
-      <h1>Template for Home component.</h1>
-      <BookSearchPage />
-      <BooksPage />
+      <div className="container">
+        <h1>Template for Home component.</h1>
+        <BookSearchPage/>
+        <BooksPage/>
+      </div>
     </div>
   );
 };
