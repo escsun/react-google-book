83fc07a3ecbda8d9241a7219c9b0aa8d2fabc349
escsun <escsunarcher@gmail.com>
2017-12-23 20:28:22 +0200

    rename router reducer

--- src/app/core/reducers/index.ts
+++ src/app/core/reducers/index.ts
@@ -1,13 +1,13 @@
-import * as fromRouter from "react-router-redux";
+import { routerReducer, RouterState } from "react-router-redux";
 import { combineReducers } from "redux";
 import * as fromBooks from "./books";
 
 export interface RootState {
-  routing: fromRouter.RouterState;
+  router: RouterState;
   books: fromBooks.BooksState;
 }
 
 export default combineReducers<RootState>({
-  routing: fromRouter.routerReducer,
+  router: routerReducer,
   books: fromBooks.booksReducer
 });
