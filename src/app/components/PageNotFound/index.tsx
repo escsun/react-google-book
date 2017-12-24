18da81a043619f98c536ff35cae610ad07af9cc4
escsun <escsunarcher@gmail.com>
2017-12-24 19:17:53 +0200

    add PageNotFound component

---
+++ src/app/components/PageNotFound/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const PageNotFound = (): JSX.Element => {
+  return (
+    <p className="page-not-found">Page not found 404.</p>
+  );
+};
+
+export default PageNotFound;
