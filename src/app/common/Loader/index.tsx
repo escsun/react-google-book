ce6641a65af2efb2c3fe2b9b73ae9af0b1fb61e4
escsun <escsunarcher@gmail.com>
2017-12-24 19:17:35 +0200

    upgrade Loader component to HOC

--- src/app/common/Loader/index.tsx
+++ src/app/common/Loader/index.tsx
@@ -2,10 +2,19 @@ import * as React from "react";
 
 import "./index.scss";
 
-const Loader = (): JSX.Element => {
-  return (
-    <div className="_loader"/>
-  );
+const isEmpty = (prop) => (
+  prop === null ||
+  prop === undefined ||
+  (prop.hasOwnProperty("length") && prop.length === 0) ||
+  (prop.constructor === Object && Object.keys(prop).length === 0)
+);
+
+const Loader = (propName: string) => (WrappedComponent: React.ComponentType<any>) => {
+  return class Loader extends React.Component<any, any> {
+    render() {
+      return isEmpty(this.props[propName]) ? <div className="loader"/> : <WrappedComponent {...this.props} />;
+    }
+  };
 };
 
 export default Loader;
