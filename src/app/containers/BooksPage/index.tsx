301d20b00ec767ab05ee49bef118a6652e11e2bb
escsun <escsunarcher@gmail.com>
2017-12-24 19:22:15 +0200

    update BooksPage

--- src/app/containers/BooksPage/index.tsx
+++ src/app/containers/BooksPage/index.tsx
@@ -1,28 +1,37 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import IBooks from "../../core/models/books.model";
 import { RootState } from "../../core/reducers";
-import { getBooks } from "../../core/selectors";
-import Books from "../../components/Books";
+import { BooksQuery } from "../../core/reducers/books";
 
-interface StateProps {
+import Books from "../../components/Books";
+import Error from "../../common/Error";
+
+import IBooks from "../../core/models/books.model";
+
+interface IStateProps {
   books: IBooks;
+  error: string;
 }
 
-class BooksPage extends React.PureComponent<StateProps, {}> {
+enum ErrorBooksMessage {
+  BooksNotFound = "Books not found",
+}
+
+class BooksPage extends React.Component<IStateProps, any> {
 
   render() {
-    const {books} = this.props;
-    return (
-      <Books books={books}/>
-    );
+    if (this.props.books && this.props.books.totalItems === 0 && !this.props.books.items) {
+      return <Error error={ErrorBooksMessage.BooksNotFound}/>;
+    }
+    return this.props.error
+      ? <Error error={this.props.error}/>
+      : <Books books={this.props.books}/>;
   }
 }
 
-const mapStateToProps = (state: RootState): StateProps => {
-  return {
-    books: getBooks(state)
-  };
-};
+const mapStateToProps = (state: RootState): IStateProps => ({
+  books: BooksQuery.getBooks(state),
+  error: BooksQuery.getBooksError(state)
+});
 
 export default connect(mapStateToProps)(BooksPage);
