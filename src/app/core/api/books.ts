69440081a4c83acb306386a81dd552dbabf8af87
escsun <escsunarcher@gmail.com>
2017-12-25 23:45:56 +0200

    add hardcoded maxResults and projection

--- src/app/core/api/books.ts
+++ src/app/core/api/books.ts
@@ -6,7 +6,10 @@ import axios, {
 import { ApiEndpointUrl } from "../constants";
 
 export const fetchGoogleBooksByQuery = (bookTitle: string) => {
-  return axios.get(ApiEndpointUrl.GOOGLE_BOOKS_BY_QUERY + bookTitle)
+  // startIndex 1 to ..
+  // maxResults 0..40
+  // projection fuLL | lite
+  return axios.get(ApiEndpointUrl.GOOGLE_BOOKS_BY_QUERY + bookTitle + "&maxResults=40&projection=full")
     .then((response: AxiosResponse) => response.data)
     .catch((error: AxiosError) => {
       throw error.response.data.error.message;
