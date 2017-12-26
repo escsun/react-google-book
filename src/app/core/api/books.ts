02d0ae603bf030c2b159e1c4fd161c67095f1f7b
escsun <escsunarcher@gmail.com>
2017-12-26 02:14:13 +0200

    update fetchGoogleBooksByQuery

--- src/app/core/api/books.ts
+++ src/app/core/api/books.ts
@@ -3,13 +3,28 @@ import axios, {
   AxiosResponse
 } from "axios";
 
+import IBooks from "../models/books.model";
+
 import { ApiEndpointUrl } from "../constants";
 
-export const fetchGoogleBooksByQuery = (bookTitle: string) => {
-  // startIndex 1 to ..
-  // maxResults 0..40
-  // projection fuLL | lite
-  return axios.get(ApiEndpointUrl.GOOGLE_BOOKS_BY_QUERY + bookTitle + "&maxResults=40&projection=full")
+/**
+ * @param bookTitle: string;
+ * @param startIndex: number; 1..n
+ * @param maxResults: string; 1..40
+ * @param projection: string; full / lite
+ * @return Promise<IBooks>
+ */
+export const fetchGoogleBooksByQuery = (bookTitle: string,
+                                        startIndex: number = 1,
+                                        maxResults: number = 40,
+                                        projection: string = "full"): Promise<IBooks> => {
+  // https://www.googleapis.com/books/v1/volumes?q=test&startIndex=1&&maxResults=40&projection=full
+  const queryUrl = ApiEndpointUrl.GOOGLE_BOOKS_BY_QUERY
+    + bookTitle
+    + "&startIndex=" + startIndex
+    + "&maxResults=" + maxResults
+    + "&projection=" + projection;
+  return axios.get(queryUrl)
     .then((response: AxiosResponse) => response.data)
     .catch((error: AxiosError) => {
       throw error.response.data.error.message;
